96094d139c8eaf29af4c281b7a35f83ce4a41caa
_ozodbek <ozodbekpanjiyev0444@gmail.com>
2025-11-20 12:08:46 +0500

    bug fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8000/api',
-    baseApi:'http://localhost:8000'
+    // apiUrl: 'http://localhost:8000/api',
+    // baseApi:'http://localhost:8000'
+    apiUrl: 'https://mytodo-backend-production.up.railway.app/api',
+    baseApi: 'https://mytodo-backend-production.up.railway.app'
+    
 }
